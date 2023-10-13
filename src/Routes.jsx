ad910a6c189814bc7d823b512512d653a88b2498
Abhishek-1405 <gorheabhi1405@gmail.com>
2023-10-13 23:48:28 +0530

    Assignment

---
+++ src/Routes.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "pages/Home";
+import NotFound from "pages/NotFound";
+const MichaelJackson = React.lazy(() => import("pages/MichaelJackson"));
+const HomePage = React.lazy(() => import("pages/HomePage"));
+const ProjectRoutes = () => {
+  return (
+    <React.Suspense fallback={<>Loading...</>}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/michaeljackson" element={<MichaelJackson />} />
+          <Route path="/dhiwise-dashboard" element={<Home />} />
+        </Routes>
+      </Router>
+    </React.Suspense>
+  );
+};
+export default ProjectRoutes;
